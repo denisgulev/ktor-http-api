d964afe284a8f378893c859777439ee004c8663a
denisgulev <denis_13@hotmail.it>
2023-11-29 23:05:14 +0100

    first commit: CustomerRoutes and OrderRoutes

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,21 @@
+package com.example.plugins
+
+import com.example.routes.customerRouting
+import com.example.routes.getOrderRoute
+import com.example.routes.listOrderRoute
+import com.example.routes.totalizeOrderRoute
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        customerRouting()
+        listOrderRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
