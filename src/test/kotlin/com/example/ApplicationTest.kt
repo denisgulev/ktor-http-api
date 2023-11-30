72a1d1574daf778063427257ed7ae195aa77ab22
denisgulev <denis_13@hotmail.it>
2023-12-01 00:51:17 +0100

    added configuration to test POST routes: need to add configuration in "kotlin-maven-plugin" to handle serialization

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,21 +1,37 @@
 package com.example
 
-import com.example.plugins.*
+import com.example.models.Customer
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello World!", bodyAsText())
+        }
+    }
+
+    @Test
+    fun testPostCustomer() = testApplication {
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+        client.post("/customer") {
+            contentType(ContentType.Application.Json)
+            setBody(Customer("1", "den", "gin", "atv@mil.ut"))
+        }.apply {
+            assertEquals("Customer stored correctly", bodyAsText())
+            assertEquals(HttpStatusCode.Created, status)
         }
     }
 }
